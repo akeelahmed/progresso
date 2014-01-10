5a28518f5a1ad7854fe091e69476da340c10fac6
Dylan Patrick Clark <dylanpatrickclark@gmail.com>
2014-01-10 13:05:40 -0800

    Add set cardinalities method.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,35 @@
 class List < ActiveRecord::Base
-  attr_accessible :board_id, :cardinality, :name, :cards_attributes
+  attr_accessible :board_id, :cardinality, :name, :ordered_card_ids
 
   belongs_to :board
   has_many :cards
-  accepts_nested_attributes_for :cards
+
+  def ordered_card_ids=(ids)
+    set_cardinalities(ids)
+  end
+
+  def set_cardinalities(ordered_ids)
+    # TODO: refactor this method.
+    values = []
+    ordered_ids.each_with_index do |unsafe_id, cardinality|
+      safe_id = unsafe_id.to_i # To prevent SQL injection.
+      values << "(#{safe_id}, #{cardinality})"
+    end
+
+    value_string = values.join(', ')
+    list_id = self.id
+
+    List.connection.execute(<<-SQL)
+      UPDATE
+        cards AS l
+      SET
+        cardinality = c.cardinality
+          FROM
+            (values #{ value_string }) as c (id, cardinality)
+      WHERE
+        c.id = l.id
+      AND
+        l.list_id = #{ list_id }
+SQL
+  end
 end
