8a5e1cfb9e4291bd3db4efad158c3f367f97e1da
Dylan Patrick Clark <dylanpatrickclark@gmail.com>
2014-01-18 22:22:52 -0800

    Add git or fetch method.

--- app/assets/javascripts/collections/boards.js
+++ app/assets/javascripts/collections/boards.js
@@ -1,5 +1,21 @@
 PRO.Collections.Boards = Backbone.Collection.extend({
     url: '/api/v1/boards',
     model: PRO.Models.Board,
-    comparator: 'id'
+    comparator: 'id',
+
+    getOrFetch: function(id) {
+        // TODO: Allow user to pass callback here.
+        var model;
+        if (!(model = this.get(id))) {
+            model = new this.model({ id: id });
+            model.fetch({
+                success: function() {
+                    this.add(model);
+                }
+            });
+        }
+        return model;
+    }
 });
+
+PRO.Store.boards = new PRO.Collections.Boards();
