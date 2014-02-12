7cac33926117aa25912ed470700d1a2af727f9a4
Dylan Patrick Clark <dylanpatrickclark@gmail.com>
2014-02-11 18:45:11 -0800

    Add touch punch hack.

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -4,7 +4,7 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// It's not advisable to add code directly here, but if you do, it'll ear at the bottom of the
 // the compiled file.
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.sortable
+//= require jquery.ui.touch-punch
 //= require underscore
 //= require backbone
 //= require progress
